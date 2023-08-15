d2bb22f3638c2a4c8bceaca27eaa55287cf5dc1a
Ezra <ezra@ezrarubio.com>
2023-08-15 19:48:37 +0300

    wip: wip

--- music-library-extractor/Models/Song.swift
+++ music-library-extractor/Models/Song.swift
@@ -9,16 +9,29 @@ import Foundation
 import AppKit
 
 struct Song: Codable, Identifiable {
-    let title: String
-    let artist: String
-    let album: String
-    let genre: String
-    let totalTime: Int
-    let trackNumber: Int
-    let sampleRate: Int
-    let purchased: Bool
-    let releaseDate: Date
-    let releaseYear: Int
+    var title: String
+    var artist: String
+    var album: String
+    var genre: String
+    var totalTime: Int
+    var trackNumber: Int
+    var sampleRate: Int
+    var purchased: Bool
+    var releaseDate: Date
+    var releaseYear: Int
     
     var id = UUID()
+    
+    private enum CodingKeys: Int, CodingKey {
+        case title = 0
+        case artist = 1
+        case album = 2
+        case genre = 3
+        case totalTime = 4
+        case trackNumber = 5
+        case sampleRate = 6
+        case purchased = 7
+        case releaseDate = 8
+        case releaseYear = 9
+    }
 }
--- music-library-extractor/ViewModels/ExportViewViewModel.swift
+++ music-library-extractor/ViewModels/ExportViewViewModel.swift
@@ -6,18 +6,19 @@
 //
 
 import Foundation
+import CodableCSV
 
 class ExportViewViewModel: ObservableObject {
-    @Published var includeTitle: Bool = false
-    @Published var includeArtist: Bool = false
-    @Published var includeAlbum: Bool = false
-    @Published var includeGenre: Bool = false
-    @Published var includeTotalTime: Bool = false
-    @Published var includeTrackNumber: Bool = false
-    @Published var includeSampleRate: Bool = false
-    @Published var includePurchased: Bool = false
-    @Published var includeReleaseDate: Bool = false
-    @Published var includeReleaseYear: Bool = false
+    @Published var includeTitle: Bool = true
+    @Published var includeArtist: Bool = true
+    @Published var includeAlbum: Bool = true
+    @Published var includeGenre: Bool = true
+    @Published var includeTotalTime: Bool = true
+    @Published var includeTrackNumber: Bool = true
+    @Published var includeSampleRate: Bool = true
+    @Published var includePurchased: Bool = true
+    @Published var includeReleaseDate: Bool = true
+    @Published var includeReleaseYear: Bool = true
     
     
     func exportCSV(_ items: [Song]) -> Void {
@@ -25,7 +26,63 @@ class ExportViewViewModel: ObservableObject {
             print("extract library first")
             return
         }
-        print(includeGenre, includeAlbum, includeTitle, includeArtist, includePurchased, includeTotalTime, includeSampleRate, includeReleaseYear, includeReleaseDate, includeTrackNumber)
-        print(items[0])
+        
+
+        let headers = declareCsvHeaders()
+        let filePath = NSHomeDirectory() + "/Desktop/Songs.csv"
+        let isFileCreated = createFile(filePath)
+        let encoder = CSVEncoder { $0.headers = headers }
+
+        do {
+            if isFileCreated {
+//                try encoder.encode(<#T##value: Encodable##Encodable#>, into: <#T##Data.Type#>)
+            } else {
+                print("there was an error creating the file.")
+            }
+        } catch {
+            print("Data encoding failed: \(error)")
+        }
+
+    }
+    
+    private func createFile(_ filePath: String) -> Bool {
+        return FileManager.default.createFile(atPath: filePath, contents: nil, attributes: nil)
+    }
+    
+    private func declareCsvHeaders() -> [String] {
+        var headers: [String] = []
+        
+        if self.includeTitle {
+            headers.append("Title")
+        }
+        if self.includeArtist {
+            headers.append("Artist")
+        }
+        if self.includeAlbum {
+            headers.append("Album")
+        }
+        if self.includeGenre {
+            headers.append("Genre")
+        }
+        if self.includeTotalTime {
+            headers.append("Length (ms)")
+        }
+        if self.includeTrackNumber {
+            headers.append("Track #")
+        }
+        if self.includeSampleRate {
+            headers.append("Sample Rate")
+        }
+        if self.includePurchased {
+            headers.append("Purchased")
+        }
+        if self.includeReleaseDate {
+            headers.append("Release Date")
+        }
+        if self.includeReleaseYear {
+            headers.append("Release Year")
+        }
+        
+        return headers
     }
 }
