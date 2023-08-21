5ba6b57e3ea84676c45ca5580903830b2df727ea
Ezra <81970147+ezraRubio@users.noreply.github.com>
2023-08-21 17:17:13 +0300

    file name by user input
    * bf: no folder creation when exists
    
    * select export file name

--- music-library-extractor/Views/ExportView.swift
+++ music-library-extractor/Views/ExportView.swift
@@ -13,36 +13,54 @@ struct ExportView: View {
     
     var body: some View {
         VStack {
-            Spacer()
-            Text("Select what data you want to include on your export: ")
-            HStack {
-                Toggle("Title", isOn: $viewModel.includeTitle)
-                Toggle("Artist", isOn: $viewModel.includeArtist)
-                Toggle("Genre", isOn: $viewModel.includeGenre)
-                Toggle("Total Time", isOn: $viewModel.includeTotalTime)
+            VStack {
+                Text("Select what data you want to include on your export: ")
+                    .padding(.top, 30)
+                
+                HStack {
+                    Toggle("Title", isOn: $viewModel.includeTitle)
+                    Toggle("Artist", isOn: $viewModel.includeArtist)
+                    Toggle("Genre", isOn: $viewModel.includeGenre)
+                    Toggle("Total Time", isOn: $viewModel.includeTotalTime)
+                }
+                HStack {
+                    Toggle("Album", isOn: $viewModel.includeAlbum)
+                    Toggle("Track Number", isOn: $viewModel.includeTrackNumber)
+                }
+                HStack {
+                    Toggle("Release Date", isOn: $viewModel.includeReleaseDate)
+                    Toggle("Released Year", isOn: $viewModel.includeReleaseYear)
+                }
+                HStack {
+                    Toggle("Sample Rate", isOn: $viewModel.includeSampleRate)
+                    Toggle("Purchased", isOn: $viewModel.includePurchased)
+                }
             }
-            HStack {
-                Toggle("Album", isOn: $viewModel.includeAlbum)
-                Toggle("Track Number", isOn: $viewModel.includeTrackNumber)
+            VStack {
+                Text("Name for the exported file: ")
+                    .padding(.top, 50)
+                TextField("File Name", text: $viewModel.fileName)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .autocorrectionDisabled()
+                    .padding([.leading, .trailing], 200)
+                
+                if !viewModel.error.isEmpty {
+                    Text(viewModel.error)
+                        .foregroundColor(.red)
+                }
+                Button("Export as .csv") {
+                    viewModel.exportCSV(libraryViewModel.mediaItems)
+                }
+                .disabled(libraryViewModel.mediaItems.isEmpty)
+                if viewModel.isFileCreated {
+                    Text("Export file successfuly created")
+                        .foregroundColor(.green)
+                }
+                if libraryViewModel.mediaItems.isEmpty {
+                    Text("First you have to extract the music library.")
+                }
             }
-            HStack {
-                Toggle("Release Date", isOn: $viewModel.includeReleaseDate)
-                Toggle("Released Year", isOn: $viewModel.includeReleaseYear)
-            }
-            HStack {
-                Toggle("Sample Rate", isOn: $viewModel.includeSampleRate)
-                Toggle("Purchased", isOn: $viewModel.includePurchased)
-            }
-            
-            Spacer()
-            Button("Export as .csv") {
-                viewModel.exportCSV(libraryViewModel.mediaItems)
-            }
-            .disabled(libraryViewModel.mediaItems.isEmpty)
-            if libraryViewModel.mediaItems.isEmpty {
-                Text("First you have to extract the music library.")
-            }
-            Spacer()
+            .padding()
         }
     }
 }
