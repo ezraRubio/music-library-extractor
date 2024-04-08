fb72c0500e1a27bcad4e736089d388e559fff412
Ezra <81970147+ezraRubio@users.noreply.github.com>
2024-04-08 12:12:20 +0300

    wip: user select to add item to spotify

---
+++ music-library-extractor/ViewModels/ToSpotifyViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ToSpotifyViewModel.swift
+//  music-library-extractor
+//
+//  Created by עזרא רוביו on 28/03/2024.
+//
+
+import Foundation
+
+class ToSpotifyViewModel {
+
+}
--- music-library-extractor/Views/ToSpotifyView.swift
+++ music-library-extractor/Views/ToSpotifyView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ToSpotifyView: View {
     let toSpotifyItems: [SpotifyLibItem]
+    @State private var selectedItems: [Bool] = []
     
     var body: some View {
         Table(toSpotifyItems) {
@@ -17,6 +18,36 @@ struct ToSpotifyView: View {
             TableColumn("Album", value: \.album)
             TableColumn("Not Found on Spotify", value: \.formattedNotFoundOnSpotify)
             TableColumn("Already in your Spotify Library", value: \.formattedOnUserLibrary)
+            TableColumn("Add to Spotify Library") { item in
+                if let index = self.toSpotifyItems.firstIndex(of: item) {
+                    Toggle("", isOn: Binding<Bool>(
+                        get: {
+                            guard index < self.selectedItems.count else { return false }
+                            return self.selectedItems[index]
+                        },
+                        set: { newValue in
+                            self.updateSelection(for: item, with: newValue)
+                        }
+                    ))
+                    .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                } else {
+                    // Handle case where item is not found
+                    EmptyView()
+                }
+            }
+        }
+    }
+    
+    private func updateSelection(for item: SpotifyLibItem, with newValue: Bool) {
+        if let index = self.toSpotifyItems.firstIndex(of: item) {
+            if index < self.selectedItems.count {
+                self.selectedItems[index] = newValue
+            } else {
+                // If the selected array doesn't have enough elements,
+                // expand it and then set the new value
+                self.selectedItems.append(contentsOf: Array(repeating: false, count: index - self.selectedItems.count + 1))
+                self.selectedItems[index] = newValue
+            }
         }
     }
 }
